3a088d17e12b357c2d79898f4fc2a95bd9f05570
Bianca Kendall <[D)>
2022-06-01 15:19:56 +0200

    added car and dealership constructers, and tests for their getters

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const Dealership = function(name, capacity, stock) {
     let _name = name;
     let _capacity = capacity;
     let _stock = stock;
-    
     // getters
     this.getName = () => _name;
     this.getCapacity = () => _capacity;
@@ -26,19 +25,37 @@ const Dealership = function(name, capacity, stock) {
     this.setName = (name) => _name = name;
     this.setCapacity = (capacity) => _capacity = capacity;
     this.setStock = (stock) => _stock = stock; 
+    // toString
+    // this.toString = () => {
+    //     return "Car {"
+    //         + "this.getmanufacturer = " + this.getManufacturer.toString()
+    //         + "this.getCapacity = " + this.getCapacity.toString()
+    //         + "this.getStock = " + this.getStock.toString()
+    //         + "}";
+    // }
 }
 
+// new car variable
+const car1 = new Car("Ford", 10000, "2-cylinder");
+const car2 = new Car("VW", 20000, "4-cylinder");
+const car3 = new Car("BMW", 30000, "4-cylinder");
+// new dealership
+const dealership = new Dealership("ABC dealers", 10, [car1, car2, car3]);
+
+console.log(car1.getManufacturer());
+
 // Count the number of cars in stock
+
+//const countStock = function(dealership) => dealership.getStock().length;
+
 // Add a car to stock
 // Return an array containing each car's manufacturer
 // Find all the cars from a given manufacturer
 // Find the total value of all the cars in stock
 
 module.exports = { 
-    getManufacturer,
-    getPrice,
-    getEngineType,
-    getName,
-    getCapacity,
-    getStock
+    car1,
+    car2,
+    car3,
+    dealership
 };
--- index.test.js
+++ index.test.js
@@ -1,17 +1,47 @@
 const {
-    getManufacturer,
-    getPrice,
-    getEngineType,
-    getName,
-    getCapacity,
-    getStock
+    car1,
+    car2,
+    car3,
+    dealership
   } = require("./index");
 
-  describe('getManufacturer', () => { 
-  
-    test('can add two small positive numbers', () => {
-      expected = 5;
-      actual = sum(2, 3);
+
+describe('get car properties', () => { 
+    test('can get manufacturer', () => {
+      expected = "Ford";
+      actual = car1.getManufacturer();
       expect(actual).toBe(expected);
     });
+
+    test('can get price', () => {
+        expected = 20000;
+        actual = car2.getPrice();
+        expect(actual).toBe(expected);
+      });
+
+      test('can get engine type', () => {
+        expected = "4-cylinder";
+        actual = car3.getEngineType();
+        expect(actual).toBe(expected);
+      });
 });    
+
+describe('get dealership properties', () => { 
+    test('can get name', () => {
+      expected = "ABC dealers";
+      actual = dealership.getName();
+      expect(actual).toBe(expected);
+    });
+
+    test('can get price', () => {
+        expected = 10;
+        actual = dealership.getCapacity();
+        expect(actual).toBe(expected);
+      });
+
+    //   test('can get stock', () => {
+    //     expected = ;
+    //     actual = car3.getEngineType();
+    //     expect(actual).toBe(expected);
+    //   });
+});  
